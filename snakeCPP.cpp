85adbcc1821138d22a845cf509bfef19db0c479b
jpromanonet <programming@jpromano.net>
2020-04-10 00:13:41 -0300

    Keys

--- snakeCPP.cpp
+++ snakeCPP.cpp
@@ -1,20 +1,46 @@
 // snakeCPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
+// Calling libraries and namespaces
 #include <iostream>
+#include <list>
+#include <thread>
+using namespace std;
+#include <Windows.h>
 
+// Defining the console screen size
+int nScreenWidth = 120;
+int nScreenHeight = 30;
+
+// Defining the snake position using an structure
+struct sSnakeSegment {
+	int x;
+	int y;
+};
+
+// The game!
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Create Screen Buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for(int i = 0; i < nScreenWidth*nScreenHeight; i++) screen[i] = L' ':
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
+
+	while (1) {
+		// Reset to known state
+		list<sSnakeSegment> snake = { {60,15},{61,15},{62,15},{63,15},{64,15},{65,15},{66,15},{67,15},{68,15},{69,15} };
+		int nFoodX = 30;
+		int nFoodY = 15;
+		int nScore = 0;
+		int nSnakeDirection = 3;
+		bool bDead = false;
+		bool bKeyLeft = false, bKeyRight = false, bKeyLeftOld = false, bKeyRightOld = false;
+
+		while (!bDead) {
+
+		}
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
